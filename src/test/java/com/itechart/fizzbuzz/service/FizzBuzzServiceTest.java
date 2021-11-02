bf0f645bad36b32fa38efee039b0e3ccc65bb6ee
Galayy <sashagalay99@yandex.ru>
2021-11-02 12:03:50 +0300

    Update tests with common case with divisors map

--- src/test/java/com/itechart/fizzbuzz/service/FizzBuzzServiceTest.java
+++ src/test/java/com/itechart/fizzbuzz/service/FizzBuzzServiceTest.java
@@ -2,7 +2,6 @@ package com.itechart.fizzbuzz.service;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 import java.util.Map;
 import java.util.stream.IntStream;
 
@@ -14,13 +13,42 @@ public class FizzBuzzServiceTest {
     private final FizzBuzzService fizzBuzzService = new FizzBuzzService();
 
     @Test
-    public void processNumbersTestWithInitialCase() {
+    public void processNumbersWithInitialCase() {
         //GIVEN
         var numbers = IntStream.rangeClosed(5, 15).boxed().collect(toList());
+        var replacementByDivisor = Map.of(3, "Fizz", 5, "Buzz");
         var expected = "Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz";
 
         //WHEN
-        var result = fizzBuzzService.processNumbers(numbers);
+        var result = fizzBuzzService.processNumbers(numbers, replacementByDivisor);
+
+        //THEN
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void processNumbersWithUnsortedInitialCase() {
+        //GIVEN
+        var numbers = IntStream.rangeClosed(5, 15).boxed().collect(toList());
+        var replacementByDivisor = Map.of(5, "Buzz", 3, "Fizz");
+        var expected = "Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz";
+
+        //WHEN
+        var result = fizzBuzzService.processNumbers(numbers, replacementByDivisor);
+
+        //THEN
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void processNumbersWithUnsortedDivisorsMap() {
+        //GIVEN
+        var numbers = IntStream.rangeClosed(5, 15).boxed().collect(toList());
+        var replacementByDivisor = Map.of(2, "New", 5, "Buzz", 3, "Fizz");
+        var expected = "Buzz NewFizz 7 New Fizz NewBuzz 11 NewFizz 13 New FizzBuzz";
+
+        //WHEN
+        var result = fizzBuzzService.processNumbers(numbers, replacementByDivisor);
 
         //THEN
         assertEquals(expected, result);
